e9f5476d48917dacebb804cd34f6fbf2e2a8302f
Serhii Lukianenko <lukyanenko88@gmail.com>
2021-10-25 20:42:08 +0300

    v0.1

--- src/main/java/com/example/myapp/TestServlet.java
+++ src/main/java/com/example/myapp/TestServlet.java
@@ -9,7 +9,7 @@ public class TestServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Test";
+        message = "Surprise!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -19,6 +19,7 @@ public class TestServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("dex.jpg");
         out.println("</body></html>");
     }
 
