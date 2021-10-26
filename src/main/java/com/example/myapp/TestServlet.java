ff77bc343af1f88be4123e46ffc91795c5ca1aea
Serhii Lukianenko <lukianenko88@gmail.com>
2021-10-26 14:15:49 +0300

    v0.7

--- src/main/java/com/example/myapp/TestServlet.java
+++ src/main/java/com/example/myapp/TestServlet.java
@@ -20,8 +20,12 @@ public class TestServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<img src=\"images/dex.jpg\">");
         out.println("<h1>" + message + "</h1>");
-        out.println("<h6>" + message + "</h6>");
+        out.println("<h2>" + message + "</h2>");
         out.println("<h3>" + message + "</h3>");
+        out.println("<h4>" + message + "</h4>");
+        out.println("<h5>" + message + "</h5>");
+        out.println("<h6>" + message + "</h6>");
+
         out.println("<img src=\"images/flame.gif\">");
 
         out.println("</body></html>");
